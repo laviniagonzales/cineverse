6956aac85596fbe58b50b72d11ca0374ab4bd968
Lavinia Gonzales <laviniagondelima@gmail.com>
2025-06-16 22:11:40 -0300

    Projeto inicial Cineverse - Spring Boot

---
+++ src/test/java/com/cineverse/CineverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
